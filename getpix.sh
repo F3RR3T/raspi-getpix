f0182ef756360ec70addd23c44c37df3df62fcf7
st33v <github@f3rr3t.com>
2016-01-06 15:19:05 +1100

    Fix false failure messages back to systemctl

--- getpix.sh
+++ getpix.sh
@@ -7,3 +7,5 @@ campi="neatherd"
 
 scp $campi:/home/st33v/pix/*.jpg /home/st33v/cams/$campi/. 2>/dev/null
 ssh $campi "rm /home/st33v/pix/*.jpg" 2>/dev/null
+
+#exit(0)     # force succes exit code for fussy systemd
